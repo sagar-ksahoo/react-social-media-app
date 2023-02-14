4f8f616d7830bbd7f02770a6e17a0c1dcd5750e5
sagar-ksahoo <sagarsahoo1729@gmail.com>
2023-02-14 20:22:50 +0530

    comment section created

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,7 +5,7 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
-// import Comments from "../comments/Comments";
+import Comments from "../comments/Comments";
 import { useState } from "react";
 const Post = ({ post }) => {
   const [commentOpen, setCommentOpen] = useState(false);
